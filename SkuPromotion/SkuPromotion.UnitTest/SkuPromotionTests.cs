ed2a8f5541acb0e0b9e855ab003e9d582b91155b
Lokesh Sharma <lokesh.sharma@tarento.com>
2020-08-18 16:53:09 +0530

    Added Project Sku Promotion Unit Test for test driven development, added three scenario with assert pass, ignored bin, vs and obj files

---
+++ SkuPromotion/SkuPromotion.UnitTest/SkuPromotionTests.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+
+namespace SkuPromotion.UnitTest
+{
+    public class SkuPromotionTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestCase_ScenarioA()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestCase_ScenarioB()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestCase_ScenarioC()
+        {
+            Assert.Pass();
+        }
+    }
+}
